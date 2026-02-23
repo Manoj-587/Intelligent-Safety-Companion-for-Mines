738468ec6a0da3dc659961b00b0f4a5a9aaec174
Manoj-587 <mkumar07959@gmail.com>
2026-02-23 21:59:32 +0530

    first commit

---
+++ mine-safety-backend/src/main/java/com/minesafety/MineSafetyBackendApplication.java
@@ -0,0 +1,14 @@
+package com.minesafety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MineSafetyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MineSafetyBackendApplication.class, args);
+        System.out.print("Hello");
+    }
+
+}
---
+++ mine-safety-backend/src/test/java/com/minesafety/MineSafetyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minesafety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MineSafetyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

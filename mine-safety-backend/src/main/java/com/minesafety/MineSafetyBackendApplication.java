932be13b6d93eac706052c9584396c80b6897e78
Manoj-587 <mkumar07959@gmail.com>
2026-02-28 11:39:43 +0530

    lombok error resolved

--- mine-safety-backend/src/main/java/com/minesafety/MineSafetyBackendApplication.java
+++ mine-safety-backend/src/main/java/com/minesafety/MineSafetyBackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MineSafetyBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MineSafetyBackendApplication.class, args);
         System.out.print("Hello");
